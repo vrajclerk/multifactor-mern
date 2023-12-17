94fd68f13fcca09632dbbc81cd13bcefa1afebf9
Vraj Clerk <vrajclerk04@gmail.com>
2023-12-18 00:07:02 +0530

    First Commit

---
+++ client/auth/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Login from './Login';
+
+function App() {
+  return (
+    <div className="App">
+           <div style={centerStyle}>
+                <h1>
+                    MFA using
+                    MERN Stack
+                </h1>
+            </div>
+            <Login />
+    </div>
+  );
+}
+const centerStyle = {
+  textAlign: 'center',
+};
+
+export default App;
