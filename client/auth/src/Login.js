bb76c4deda5a23aaa4f938cc0e0f1609ae57b158
Vraj Clerk <vrajclerk04@gmail.com>
2023-12-18 23:42:33 +0530

    half

--- client/auth/src/Login.js
+++ client/auth/src/Login.js
@@ -13,7 +13,7 @@ const Login = () => {
 		try {
 			const response =
 				await axios.post(
-					'http://localhost:3001/auth/login',
+					'http://localhost:3001/auth/Login',
 					{
 						email,
 						password
